6b90ce3fc8b2ba63ade16aaf8e05b2710fcc0106
aamirakhtar <aamiradvantage@gmail.com>
2020-10-06 05:36:28 +0500

    Assemblies and GAC

---
+++ DataAccessLayer/DAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DAL
+    {
+        public List<string> GetDataFromDB(string connectionString)
+        {
+            return new List<string>();
+        }
+
+        public void SaveDataToDB(string data)
+        {
+            Console.WriteLine("data saved to db");
+        }
+    }
+}
--- OOPWithCSharp/Interface.cs
+++ OOPWithCSharp/Interface.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,10 @@ namespace OOPWithCSharp
         {
             MDDScanner mddScanner = new MDDScanner(new JsonLogger());
             mddScanner.ScanBarcode();
+
+            DAL dal = new DAL();
+            dal.GetDataFromDB("connection string");
+            dal.SaveDataToDB("data");
 
             Console.ReadLine();
         }
